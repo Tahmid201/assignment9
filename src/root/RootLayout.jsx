a4c5ae245bc6b7f7184c7bbdf778fffb23343a0f
Tahmid201 <abdusshafitahmid635@gmail.com>
2025-11-27 14:14:42 +0600

    updated

--- src/root/RootLayout.jsx
+++ src/root/RootLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { ToastContainer } from 'react-toastify';
 
 const RootLayout = () => (
   <div className="min-h-screen flex flex-col">
@@ -10,7 +11,10 @@ const RootLayout = () => (
       <Outlet />
     </main>
     <Footer />
+    <ToastContainer></ToastContainer>
   </div>
+  
 );
+
 
 export default RootLayout;
